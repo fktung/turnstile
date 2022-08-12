7b193af5bc1de4bc10d71bb05c0479d0241e38cf
Dwi Nofebri <dwinofebri@gmail.com>
2022-08-12 16:27:50 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,52 @@
+# | py -3 -m json.tool
 import requests
 import json
+pin=4
+import RPi.GPIO as GPIO
+import time
 
-parload = {'quert': 'Cyrus'}
-response = requests.get("https://api.urbanathletes.co.id/fitness/v1/branch")
-# print(response.json())
-# print(json.dumps(response.json()))
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pin, GPIO.OUT)
 
-branchData = response.json()['data']['rows']
+while True:
+    try:
+        rfidMember = input("RFID = ")
+        dataReq = {"rfid_card_code": rfidMember}
+        response = requests.post("https://api.urbanathletes.co.id/fitness/v1/scanning/gym_attendance", data=dataReq)
+        # print(response.json())
+        # print(json.dumps(response.json()))
 
-# print(branchData)
-print(json.dumps(branchData))
-for branch in branchData:
-  print(branch['name'])
+        memberData = response.json()
+
+        #print(memberData['member']['email'])
+        if rfidMember == '^C' or rfidMember == 'exit':
+            break
+        elif memberData['member']:
+            GPIO.output(pin, False)
+            print('on')
+            time.sleep(1)
+            GPIO.output(pin, True)
+            print('off')
+            time.sleep(0.3)
+            print('RFID Terdaftar')
+            continue
+        else:
+            print('RFID Tidak terdaftar')
+            continue
+    except:
+        print("error")
+#KeyboardInterrupt(False)
+GPIO.cleanup()
+
+
+
+
+
+
+
+
+
+
+
+
