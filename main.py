10835ffd5769952af0804a7b33e20f157c2aefad
Febry Kentung <34118435+fktung@users.noreply.github.com>
2022-09-16 19:42:54 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,52 +1,63 @@
-# | py -3 -m json.tool
+club=1  ##> Ganti Code club sesuai club
 import requests
 import json
-pin=4
+pinIn=17
+pinOut=4
 import RPi.GPIO as GPIO
 import time
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(pin, GPIO.OUT)
+GPIO.setup(pinIn, GPIO.OUT)
+GPIO.setup(pinOut, GPIO.OUT)
 
 while True:
     try:
         rfidMember = input("RFID = ")
-        dataReq = {"rfid_card_code": rfidMember}
-        response = requests.post("https://api.urbanathletes.co.id/fitness/v1/scanning/gym_attendance", data=dataReq)
-        # print(response.json())
-        # print(json.dumps(response.json()))
-
-        memberData = response.json()
-
-        #print(memberData['member']['email'])
-        if rfidMember == '^C' or rfidMember == 'exit':
+        # dataReq = {"rfid": rfidMember, "branch_id": club}
+        # response = requests.post("http://dev-web.urbanathletes.co.id/api/v1/turnstile", data=dataReq)
+        # memberData = response.json()
+        if rfidMember == 'EXIT' or rfidMember == 'exit':
             break
-        elif memberData['member']:
-            GPIO.output(pin, False)
+        if rfidMember == 'StaffUrban1234':
+            GPIO.output(pinIn, False)
+            GPIO.output(pinOut, False)
             print('on')
             time.sleep(1)
-            GPIO.output(pin, True)
+            GPIO.output(pinIn, True)
+            GPIO.output(pinOut, True)
             print('off')
             time.sleep(0.3)
-            print('RFID Terdaftar')
+            print('Staff')
+            continue
+            
+        dataReq = {"rfid": rfidMember, "branch_id": club}
+        response = requests.post("http://dev-web.urbanathletes.co.id/api/v1/turnstile", data=dataReq)
+        memberData = response.json()
+        if memberData["open"] == "OUT":
+            GPIO.output(pinOut, False)
+            print('on')
+            time.sleep(1)
+            GPIO.output(pinOut, True)
+            print('off')
+            time.sleep(0.3)
+            print(memberData)
+            continue
+        elif memberData["open"] == "IN":
+            GPIO.output(pinIn, False)
+            print('on')
+            time.sleep(1)
+            GPIO.output(pinIn, True)
+            print('off')
+            time.sleep(0.3)
+            print(memberData)
             continue
         else:
             print('RFID Tidak terdaftar')
             continue
     except:
-        print("error")
+        print("error turnstile")
+#        print(error)
+#        break
 #KeyboardInterrupt(False)
 GPIO.cleanup()
-
-
-
-
-
-
-
-
-
-
-
-
